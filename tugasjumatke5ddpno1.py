3b48b021cb7593d4f015bfc5e2292858018e1a5d
FadilAzzam <fadilazzam0@gmail.com>
2024-11-04 19:27:36 +0700

    MuhamadFadilAzzam_0110124029_SI09

---
+++ tugasjumatke5ddpno1.py
@@ -0,0 +1,11 @@
+kendaraan = ["beat street", "motor", "110 cc", "warna silver", "Roda 2"]
+print(kendaraan)
+
+kendaraan.append("Harga 20jt")
+print(kendaraan)
+
+kendaraan.append("tipe matic")
+print(kendaraan)
+
+kendaraan.insert(2, "Honda")
+print(kendaraan)
